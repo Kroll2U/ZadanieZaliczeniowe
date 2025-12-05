25b9d120c584a7165d6600d6aebfa9b9bcca17d0
Szaski <szaski206@gmail.com>
2025-12-05 13:28:09 +0100

    Pobranie zmiennych do javy oraz dodanie stringa generatedPassword

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextFirstName, editTextLastName, editTextPasswordLength;
+    Spinner spinnerPosition;
+    CheckBox checkBoxLetters, checkBoxDigits, checkBoxSpecial;
+    Button buttonGenerate, buttonSubmit;
+
+    String generatedPassword = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextPasswordLength = findViewById(R.id.editTextPasswordLength);
+
+        spinnerPosition = findViewById(R.id.spinnerPosition);
+
+        checkBoxLetters = findViewById(R.id.checkBoxLetters);
+        checkBoxDigits = findViewById(R.id.checkBoxDigits);
+        checkBoxSpecial = findViewById(R.id.checkBoxSpecial);
+
+        buttonGenerate = findViewById(R.id.buttonGenerate);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
     }
 }
